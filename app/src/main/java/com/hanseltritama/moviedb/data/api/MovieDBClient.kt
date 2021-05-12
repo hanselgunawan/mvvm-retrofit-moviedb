bf8b44c72374b1c017878a6bdce1dc8fd95cd23e
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-05-12 13:43:59 -0700

    adding PageKeyedDataSource

--- app/src/main/java/com/hanseltritama/moviedb/data/api/MovieDBClient.kt
+++ app/src/main/java/com/hanseltritama/moviedb/data/api/MovieDBClient.kt
@@ -11,6 +11,9 @@ const val API_KEY = "1b6bccfa81825771cd296691cf2de775"
 const val BASE_URL = "https://api.themoviedb.org/3/"
 const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342/"
 
+const val FIRST_PAGE = 1
+const val POST_PER_PAGE = 20
+
 object MovieDBClient {
 
     fun getClient(): MovieDBInterface {
---
+++ app/src/main/java/com/hanseltritama/moviedb/data/repository/MovieDataSource.kt
@@ -0,0 +1,67 @@
+package com.hanseltritama.moviedb.data.repository
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.PageKeyedDataSource
+import com.hanseltritama.moviedb.data.Movie
+import com.hanseltritama.moviedb.data.api.FIRST_PAGE
+import com.hanseltritama.moviedb.data.api.MovieDBInterface
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MovieDataSource(
+    private val apiService: MovieDBInterface,
+    private val compositeDisposable: CompositeDisposable) : PageKeyedDataSource<Int, Movie>() {
+
+    private var page = FIRST_PAGE
+    private val networkState: MutableLiveData<NetworkState> = MutableLiveData()
+
+    override fun loadInitial(
+        params: LoadInitialParams<Int>,
+        callback: LoadInitialCallback<Int, Movie>
+    ) {
+        networkState.postValue(NetworkState.LOADING)
+
+        compositeDisposable.add(
+            apiService.getPopularMovie(page)
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                    {
+                        callback.onResult(it.movieList, null, page+1)
+                        networkState.postValue(NetworkState.LOADED)
+                    },
+                    {
+                        networkState.postValue(NetworkState.ERROR)
+                        Log.e("MovieDataSource", it.message.toString())
+                    }
+                )
+        )
+    }
+
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
+        networkState.postValue(NetworkState.LOADING)
+
+        compositeDisposable.add(
+            apiService.getPopularMovie(params.key)
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                    {
+                        // we have more pages to load
+                        if (it.totalPages >= params.key) {
+                            callback.onResult(it.movieList, params.key + 1)
+                            networkState.postValue(NetworkState.LOADED)
+                        } else {
+                            networkState.postValue(NetworkState.ENDOFLIST)
+                        }
+                    },
+                    {
+                        networkState.postValue(NetworkState.ERROR)
+                        Log.e("MovieDataSource", it.message.toString())
+                    }
+                )
+        )
+    }
+
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {}
+
+}
