adfcea8497ce361224b19d5385bcd11cad784b48
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-05-12 20:37:43 -0700

    added adapter and layoutManager to the recyclerView

--- app/src/main/java/com/hanseltritama/moviedb/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/moviedb/MainActivity.kt
@@ -3,17 +3,70 @@ package com.hanseltritama.moviedb
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.hanseltritama.moviedb.data.PopularMoviePagedListAdapter
+import com.hanseltritama.moviedb.data.api.MovieDBClient
+import com.hanseltritama.moviedb.data.api.MovieDBInterface
+import com.hanseltritama.moviedb.data.repository.NetworkState
+import com.hanseltritama.moviedb.viewmodel.MainActivityViewModel
+import com.hanseltritama.moviedb.viewmodel.MovieDetailsViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainActivityViewModel
+
+    lateinit var movieRepository: MoviePagedListRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn.setOnClickListener {
-            val intent = Intent(this, MovieDetails::class.java)
-            intent.putExtra("id", 299534)
-            this.startActivity(intent)
+        val apiService: MovieDBInterface = MovieDBClient.getClient()
+
+        movieRepository = MoviePagedListRepository(apiService)
+
+        getViewModel()
+
+        val movieAdapter = PopularMoviePagedListAdapter(this)
+
+        val gridLayoutManager = GridLayoutManager(this, 3)
+
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val viewType = movieAdapter.getItemViewType(position)
+                return if (viewType == movieAdapter.MOVIE_VIEW_TYPE) 1 else 3
+            }
         }
+
+        rv_movie_list.layoutManager = gridLayoutManager
+        rv_movie_list.setHasFixedSize(true)
+        rv_movie_list.adapter = movieAdapter
+
+        viewModel.movidPagedList.observe(this, Observer {
+            movieAdapter.submitList(it)
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            progress_bar_popular.visibility = if (viewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            txt_error_popular.visibility = if (viewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+            if (!viewModel.listIsEmpty()) {
+                movieAdapter.setNetworkState(it)
+            }
+        })
+    }
+
+    private fun getViewModel(): MainActivityViewModel {
+        return ViewModelProvider(this, object: ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MainActivityViewModel(movieRepository) as T
+            }
+        })[MainActivityViewModel::class.java]
     }
 }
